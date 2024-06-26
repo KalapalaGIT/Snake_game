9f772c42a313881abfc7f5f07faf1546b5076bb4
KalapalaGIT <lassi.uk@gmail.com>
2024-06-26 17:49:32 +0300

    Update scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -12,7 +12,7 @@ class Scoreboard(Turtle):
         self.goto(x=0,y=250)
         self.color('white')
         self.score = 0
-        with open('https://github.com/KalapalaGIT/Snake_game/blob/931661467be79c4135f7b21c22b470ff54ad5862/highscore.txt', 'r') as f:
+        with open('highscore.txt', 'r') as f:
             self.hightscore = int(f.read())
 
     def Update_scoreboard(self):
@@ -26,7 +26,7 @@ class Scoreboard(Turtle):
     def Reset(self):
         if self.score > self.hightscore:
             self.hightscore = self.score
-            with open('Day20_Snake _game/Snake_game/highscore.txt', 'w') as f:
+            with open('highscore.txt', 'w') as f:
                 f.write(str(self.hightscore))
         self.score = 0
         
